e207def9daeeb0bf86c79d4415134f096e12dad3
Ivan Workbox <workboxivan@gmail.com>
2017-06-10 22:39:01 +0300

    2017_06_10

--- adminka/adminka/urls.py
+++ adminka/adminka/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from adminka.views import hello, current_datetime
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^hello/$', hello),
+    url(r'^time/$', current_datetime)
 ]
